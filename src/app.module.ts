2e2efc462a4d94a708e6c097e93752f73464c955
ClintonTSR <soonren20@icloud.com>
2025-01-20 21:12:42 +0800

    chore: setup typeorm and config deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
+import { DB_CONNECTION_NAME } from './todo/constants/db.contants';
+import { Todo } from './todo/entities/todo.entity';
 
 @Module({
-  imports: [TodoModule],
+ imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            imports: [ConfigModule],
+            name: DB_CONNECTION_NAME,
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('DB_HOST'),
+                port: configService.get('DB_PORT'),
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_DATABASE'),
+                synchronize: true, //configService.get('NODE_ENV') === 'development',
+
+            }),
+        }),],
   controllers: [AppController],
   providers: [AppService],
 })
