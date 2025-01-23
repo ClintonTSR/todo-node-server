d66f1fe75a9bb062b830fd8bf682c1db7c8eee72
ClintonTSR <soonren20@icloud.com>
2025-01-23 22:40:16 +0800

    fix: missing import

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_CONNECTION_NAME } from './constants/db.contants';
+import { Todo } from './entities/todo.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature(
+    [Todo],
+    DB_CONNECTION_NAME)],
   controllers: [TodoController],
   providers: [TodoService],
 })
